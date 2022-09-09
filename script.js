0dbb54541c2ee946aff36a0946fb7542c420a9d8
mathcampos21 <108019720+mathcampos21@users.noreply.github.com>
2022-09-09 16:14:17 -0300

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -7,8 +7,25 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-passwords");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGJIJKLMNOPQRSTUVWXYZ0123456789!@#";
+let checkboxMinuscula = document.querySelector("#input-minuscula");
+let checkboxMaiuscula = document.querySelector("#input-maiuscula");
+let checkboxNumero = document.querySelector("#input-numeros");
+let checkboxSimbolo = document.querySelector("#input-simbolos");
+
+let button = document.querySelector("#button")
+
+
+
+let Selecionados = 0;
+
+let minuscula = "abcdefghijklmnopqrstuvwxyz";
+let maiuscula = "ABCDEFGJIJKLMNOPQRSTUVWXYZ";
+let simbolos = "!@#$%&*";
+let numeros = "0123456789";
+
+
 let novaSenha = "";
+
 
 sizePassword.innerHTML = sliderElement.value;
 
@@ -17,8 +34,34 @@ slider.oninput = function(){
 }
 
 function generatepassword(){
-     
+
     let pass = "";
+    let charset
+
+    if (checkboxMinuscula.checked) {
+        charset += minuscula
+    } else {
+        charset -= minuscula
+    }
+
+    if (checkboxMaiuscula.checked) {
+        charset += maiuscula
+    } else {
+        charset -= maiuscula
+    }
+
+    if (checkboxNumero.checked) {
+        charset += numeros
+    } else {
+        charset -= numeros
+    }
+
+    if (checkboxSimbolo.checked) {
+        charset += simbolos
+    } else {
+        charset -= simbolos
+    }
+
 
     for (let i = 0, n = charset.length; i < sliderElement.value; ++i){
         pass += charset.charAt(Math.floor(Math.random() * n));
@@ -34,3 +77,5 @@ function copyPassword(){
     navigator.clipboard.writeText(novaSenha);
 
 }
+
+
